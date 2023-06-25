0130a881a427245b549820763f00f95d6494d3ba
vanlo <vanlo@DESKTOP-87PONBL>
2023-06-25 08:50:11 +0700

    Add project files.

---
+++ Decrypt/Program.cs
@@ -0,0 +1,139 @@
+﻿// -------------NOTE----------------//
+// You must be rebuild solution     //
+//----------------------------------//
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Runtime.InteropServices;
+
+
+class Program
+{
+    [DllImport("kernel32.dll")]
+    private static extern bool AllocConsole();
+
+    [DllImport("kernel32.dll")]
+    private static extern bool FreeConsole();
+
+    [DllImport("user32.dll")]
+    private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+    private const int SW_HIDE = 0;
+    static void Main()
+    {
+        // Tạo cửa sổ console
+        AllocConsole();
+
+        // Ẩn cửa sổ console
+        IntPtr consoleWindow = Process.GetCurrentProcess().MainWindowHandle;
+        ShowWindow(consoleWindow, SW_HIDE);
+        // Đường dẫn tệp tin .ico chứa dữ liệu đã được dấu
+        string inputIconFile = "Decrypt.exe";
+        AllocConsole();
+
+
+        // Trích xuất dữ liệu từ icon
+        string encodedData = ExtractDataFromIcon(inputIconFile);
+        string[] parts = encodedData.Split('|');
+
+
+        // Giải mã Base64 để lấy lại giá trị key, nonce và tag
+        byte[] key = Convert.FromBase64String(parts[0]);
+        byte[] nonce = Convert.FromBase64String(parts[1]);
+        byte[] tag = Convert.FromBase64String(parts[2]);
+        File.WriteAllText("encodedData.txt", encodedData);
+
+        // Đọc tệp tin mã hóa và tag từ tệp tin đầu ra
+        byte[] ciphertext = File.ReadAllBytes("bin");
+
+        // Giải mã với AES-256-GCM
+        byte[] plaintext;
+        using (AesGcm aes = new AesGcm(key))
+        {
+            plaintext = new byte[ciphertext.Length];
+            aes.Decrypt(nonce, ciphertext, tag, plaintext);
+        }
+        File.WriteAllBytes("output.exe", plaintext);
+        FileInfo f = new FileInfo("output.exe");
+        f.Attributes = FileAttributes.Hidden;
+        // Khởi động tệp tin output.exe
+        Process process = new Process();
+        process.StartInfo.FileName = "output.exe";
+
+
+        process.Start();
+
+        // Đợi tín hiệu dừng chạy từ output.exe
+        process.WaitForExit();
+
+        // Xoá tệp tin output.exe
+        File.Delete("output.exe");
+        FreeConsole();
+    }
+
+    static string ExtractDataFromIcon(string inputIconFile)
+    {
+        // Đọc icon từ tệp tin .ico
+        Icon icon = Icon.ExtractAssociatedIcon(inputIconFile);
+
+        // Chuyển icon thành bitmap để thực hiện các thao tác pixel
+        Bitmap bitmap = icon.ToBitmap();
+        // Đọc các bit đã được dấu trong bitmap để lấy dữ liệu
+        string message = string.Empty;
+        char ch = (char)0;
+        int bitCount = 0;
+        for (int row = 0; row < bitmap.Height; row++)
+        {
+            for (int col = 0; col < bitmap.Width; col++)
+            {
+                Color pixelColor = bitmap.GetPixel(col, row);
+                for (int color = 0; color < 3; color++)
+                {
+                    if (bitCount < 8)
+                    {
+                        if (GetLSB(pixelColor, color))
+                            ch |= (char)(1 << (7 - bitCount));
+
+                        bitCount++;
+                    }
+
+                    if (bitCount == 8)
+                    {
+                        if (ch == '\0')
+                            return message;
+
+                        bitCount = 0;
+                        message += ch;
+                        ch = (char)0;
+                    }
+                }
+            }
+        }
+
+
+        return message;
+    }
+    static bool GetLSB(Color pixel, int color)
+    {
+        bool value;
+        switch (color)
+        {
+            case 0:
+                value = (pixel.R & 1) != 0;
+                break;
+            case 1:
+                value = (pixel.G & 1) != 0;
+                break;
+            case 2:
+                value = (pixel.B & 1) != 0;
+                break;
+            default:
+                throw new ArgumentException("Invalid color value. Expected values: 0 (R), 1 (G), 2 (B).");
+        }
+        return value;
+    }
+
+}
---
+++ Encrypt/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        // Đường dẫn tệp tin .exe đầu vào
+        string inputFile = "Game Co Caro.exe";
+
+        // Tạo khóa ngẫu nhiên
+        byte[] key = GenerateRandomBytes(32); // 32 byte = 256 bit
+
+        // Tạo một nonce (số duy nhất một lần)
+        byte[] nonce = GenerateRandomBytes(12); // 12 byte = 96 bit 
+
+        // Mở tệp tin đầu vào
+        byte[] plaintext = File.ReadAllBytes(inputFile);
+
+        // Mã hóa với AES-256-GCM
+        byte[] ciphertext, tag;
+        using (AesGcm aes = new AesGcm(key))
+        {
+            ciphertext = new byte[plaintext.Length];
+            tag = new byte[16]; // 16 byte = 128 bit (độ dài mặc định của tag trong AES-GCM)
+            aes.Encrypt(nonce, plaintext, ciphertext, tag);
+        }
+
+        // Ghi tệp tin mã hóa và tag vào tệp tin đầu ra
+        File.WriteAllBytes("bin", ciphertext);
+        // Chuyển đổi key, nonce và tag sang dạng Base64
+        string keyBase64 = Convert.ToBase64String(key);
+        string nonceBase64 = Convert.ToBase64String(nonce);
+        string tagBase64 = Convert.ToBase64String(tag);
+
+        // Dấu các giá trị key, nonce và tag vào icon của tệp tin đầu vào
+        string outputIconFile = "output.png";
+        EmbedDataInIcon(inputFile, outputIconFile, keyBase64, nonceBase64, tagBase64);
+
+        Console.WriteLine("Mã hóa và dấu dữ liệu thành công!");
+    }
+
+    static byte[] GenerateRandomBytes(int length)
+    {
+        byte[] randomBytes = new byte[length];
+        using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+        {
+            rng.GetBytes(randomBytes);
+        }
+        return randomBytes;
+    }
+    static void EmbedDataInIcon(string inputFile, string outputIconFile, string keyBase64, string nonceBase64, string tagBase64)
+    {
+        // Trích xuất icon từ tệp tin .exe
+        Icon icon = Icon.ExtractAssociatedIcon(inputFile);
+
+        // Ghép các chuỗi Base64 thành một chuỗi duy nhất
+        string message = $"{keyBase64}|{nonceBase64}|{tagBase64}\0";
+        File.WriteAllText("message.txt", message);
+        // Kiểm tra kích thước dữ liệu cần dấu có vượt quá giới hạn của icon hay không
+        int maxDataLength = icon.Width * icon.Height * 3 / 8; // Giới hạn dữ liệu dấu là 3/8 kích thước icon
+        if (message.Length > maxDataLength)
+        {
+            throw new Exception("Dữ liệu cần dấu vượt quá giới hạn kích thước của icon.");
+        }
+
+        // Chuyển icon thành bitmap để thực hiện các thao tác pixel
+        Bitmap bitmap = icon.ToBitmap();
+
+        // Dấu dữ liệu vào bitmap
+        int bitCount = 0;
+        int dataIndex = 0;
+        for (int j = 0; j < bitmap.Height; j++)
+        {
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                Color pixelColor = bitmap.GetPixel(i, j);
+                for (int color = 0; color < 3; color++)
+                {
+                    if (dataIndex < message.Length)
+                    {
+                        byte value = (byte)message[dataIndex];
+
+                        if (IsBitSet(value, 7 - bitCount))
+                        {
+                            pixelColor = SetLSB(pixelColor, true, color);
+                        }
+                        else
+                        {
+                            pixelColor = SetLSB(pixelColor, false, color);
+                        }
+
+                        bitCount++;
+
+                        if (bitCount == 8)
+                        {
+                            bitCount = 0;
+                            dataIndex++;
+                        }
+                    }
+                    bitmap.SetPixel(i, j, pixelColor);
+                }
+            }
+        }
+        bitmap.Save(outputIconFile);
+    }
+
+    static bool IsBitSet(byte value, int pos)
+    {
+        value = (byte)(value >> pos);
+        return (value & 1) == 1;
+    }
+
+    static Color SetLSB(Color pixel, bool bitValue, int color)
+    {
+        int lsb = bitValue ? 1 : 0;
+        int r = pixel.R;
+        int g = pixel.G;
+        int b = pixel.B;
+        switch (color)
+        {
+            case 0:
+                r = (pixel.R & ~1) | lsb;
+                break;
+            case 1:
+                g = (pixel.G & ~1) | lsb;
+                break;
+            case 2:
+                b = (pixel.B & ~1) | lsb;
+                break;
+        }
+        return Color.FromArgb(pixel.A, r, g, b);
+    }
+}
